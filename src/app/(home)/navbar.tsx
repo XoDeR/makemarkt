84ab4a05fe2eca7bd76ffe21cda5647e16f5fd22
XoDeR <xoder.vs@gmail.com>
2025-07-09 16:30:24 +0200

    added sign-in/sign-up buttons to desktop navbar

--- src/app/(home)/navbar.tsx
+++ src/app/(home)/navbar.tsx
@@ -68,6 +68,26 @@ export const Navbar = () => {
           </NavbarItem>
         ))}
       </div>
+
+      <div className="hidden lg:flex">
+        <Button
+          asChild
+          variant="secondary"
+          className="border-l border-t-0 border-b-0 border-r-0 px-12 h-full rounded-none bg-white hover:bg-pink-400 transition-colors text-lg"
+        >
+          <Link href="/sign-in">
+            Log in
+          </Link>
+        </Button>
+        <Button
+          asChild
+          className="border-l border-t-0 border-b-0 border-r-0 px-12 h-full rounded-none bg-black text-white hover:text-black hover:bg-pink-400 transition-colors text-lg"
+        >
+          <Link href="/sign-up">
+            Start selling
+          </Link>
+        </Button>
+      </div>
     </nav>
-  )
-}
+  );
+};
