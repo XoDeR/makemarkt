e19f9bc10bc681cdfb0f2ed25e9b556481d69001
XoDeR <xoder.vs@gmail.com>
2025-07-10 21:45:26 +0200

    try to load data from payload

--- src/app/(app)/(home)/page.tsx
+++ src/app/(app)/(home)/page.tsx
@@ -1,12 +1,24 @@
+import configPromise from '@payload-config'
+import { getPayload } from 'payload'
+
 import { Button } from "@/components/ui/button";
 import { Checkbox } from "@/components/ui/checkbox";
 import { Input } from "@/components/ui/input";
 import { Progress } from "@/components/ui/progress";
 import { Textarea } from "@/components/ui/textarea";
 
-export default function Home() {
+export default async function Home() {
+  const payload = await getPayload({
+    config: configPromise,
+  })
+
+  const data = await payload.find({
+    collection: "categories",
+  })
+
   return (
     <div className="p-4">
+      <div></div>
       <div className="flex flex-col gap-y-4">
         <div>
           <Button variant="elevated">
