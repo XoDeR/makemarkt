3bcb8ec108be55d5e8a3302268780a568eb3937c
XoDeR <xoder.vs@gmail.com>
2025-07-12 23:32:32 +0200

    home page: search input component

--- src/app/(app)/(home)/page.tsx
+++ src/app/(app)/(home)/page.tsx
@@ -1,5 +1,4 @@
-import configPromise from '@payload-config'
-import { getPayload } from 'payload'
+
 
 import { Button } from "@/components/ui/button";
 import { Checkbox } from "@/components/ui/checkbox";
@@ -7,24 +6,11 @@ import { Input } from "@/components/ui/input";
 import { Progress } from "@/components/ui/progress";
 import { Textarea } from "@/components/ui/textarea";
 
-export default async function Home() {
-  const payload = await getPayload({
-    config: configPromise,
-  })
-
-  const data = await payload.find({
-    collection: "categories",
-    where: {
-      parent: {
-        exists: false,
-      },
-    },
-  });
-
+export default function Home() {
   return (
     <div className="p-4">
       <div>
-        {JSON.stringify(data, null, 2)}
+        Home page
       </div>
       <div className="flex flex-col gap-y-4">
         <div>
