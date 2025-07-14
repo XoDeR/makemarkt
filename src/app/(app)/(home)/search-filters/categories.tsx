e43224aae3b9d8ecc9067f87b60b9065b4f368e9
XoDeR <xoder.vs@gmail.com>
2025-07-14 14:59:30 +0200

    added categories component

---
+++ src/app/(app)/(home)/search-filters/categories.tsx
@@ -0,0 +1,16 @@
+import { Dice1 } from "lucide-react";
+
+interface Props {
+  data: any;
+};
+
+export const Categories = ({
+  data
+}: Props) => {
+  return (
+    <div>
+      Categories
+    </div>
+
+  )
+}
--- src/app/(app)/(home)/search-filters/index.tsx
+++ src/app/(app)/(home)/search-filters/index.tsx
@@ -1,3 +1,4 @@
+import { Categories } from "./categories";
 import { SearchInput } from "./search-input";
 
 interface Props {
@@ -10,6 +11,7 @@ export const SearchFilters = ({
   return (
     <div className="px-4 lg:px-12 py-8 border-b flex flex-col gap-4 w-full">
       <SearchInput />
+      <Categories data={data} />
       {JSON.stringify(data, null, 2)}
     </div>
   )
