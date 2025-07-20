06536fb847af17d10408565032b24f005f00390f
XoDeR <xoder.vs@gmail.com>
2025-07-20 16:48:06 +0200

    workaround to make db:seed script work

--- src/seed.ts
+++ src/seed.ts
@@ -1,3 +1,7 @@
+import { getPayload } from "payload";
+import config from "@payload-config";
+import "dotenv/config";
+
 const categories = [
   {
     name: "All",
@@ -133,3 +137,34 @@ const categories = [
     ],
   },
 ]
+
+const seed = async () => {
+  const payload = await getPayload({ config });
+
+  for (const category of categories) {
+    const parentCategory = await payload.create({
+      collection: "categories",
+      data: {
+        name: category.name,
+        slug: category.slug,
+        color: category.color,
+        parent: null,
+      },
+    });
+
+    for (const subCategory of category.subcategories || []) {
+      await payload.create({
+        collection: "categories",
+        data: {
+          name: subCategory.name,
+          slug: subCategory.slug,
+          parent: parentCategory.id,
+        },
+      });
+    }
+  }
+}
+
+await seed();
+
+process.exit(0);
