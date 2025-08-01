6a76f0ec5d26a9ebf5356755735fb74cad825c60
XoDeR <xoder.vs@gmail.com>
2025-08-01 13:01:09 +0200

    logic to highlight active category (wip)

--- src/app/(app)/(home)/search-filters/categories.tsx
+++ src/app/(app)/(home)/search-filters/categories.tsx
@@ -1,6 +1,8 @@
-import { Category } from "@/payload-types";
+"use client";
+
 import { CategoryDropdown } from "./category-dropdown";
 import { CustomCategory } from "../types";
+import { useEffect, useRef, useState } from "react";
 
 interface Props {
   data: CustomCategory[];
@@ -9,6 +11,26 @@ interface Props {
 export const Categories = ({
   data
 }: Props) => {
+  const containerRef = useRef<HTMLDivElement>(null);
+  const measureRef = useRef<HTMLDivElement>(null);
+  const viewAllRef = useRef<HTMLDivElement>(null);
+
+  const [visibleCount, setVisibleCount] = useState(data.length);
+  const [isAnyHovered, setIsAnyHovered] = useState(false);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  // mock active category
+  const activeCategory = "all";
+
+  const activeCategoryIndex = data.findIndex((cat) => cat.slug === activeCategory);
+  const isActiveCategoryHidden = activeCategoryIndex >= visibleCount && activeCategoryIndex !== -1;
+  
+  useEffect(() => {
+    const calculateVisible = () => {
+
+    };
+  }, []);
+  
   return (
     <div className="relative w-full">
       <div className="flex flex-nowrap items-center">
@@ -16,7 +38,7 @@ export const Categories = ({
           <div key={category.id}>
             <CategoryDropdown
               category={category}
-              isActive={false}
+              isActive={activeCategory === category.slug}
               isNavigationHovered={false}
             />
           </div>
