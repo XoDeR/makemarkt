29f4bc4ddec2a7a5a014cb49222977e08ae42599
XoDeR <xoder.vs@gmail.com>
2025-08-17 18:43:52 +0200

    example getting data from client component

--- src/app/(app)/(home)/page.tsx
+++ src/app/(app)/(home)/page.tsx
@@ -1,20 +1,26 @@
-
+"use client";
 
 import { Button } from "@/components/ui/button";
 import { Checkbox } from "@/components/ui/checkbox";
 import { Input } from "@/components/ui/input";
 import { Progress } from "@/components/ui/progress";
 import { Textarea } from "@/components/ui/textarea";
-import { getQueryClient, trpc } from "@/trpc/server";
+//import { getQueryClient, trpc } from "@/trpc/server";
+import { useTRPC } from "@/trpc/client";
+import { useQuery } from "@tanstack/react-query";
 
-export default async function Home() {
-  const queryClient = getQueryClient();
-  const categories = await queryClient.fetchQuery(trpc.categories.getMany.queryOptions());
-  
+export default function Home() {
+  //const queryClient = getQueryClient();
+  //const categories = await queryClient.fetchQuery(trpc.categories.getMany.queryOptions());
+  const trpc = useTRPC();
+  const categories = useQuery(trpc.categories.getMany.queryOptions());
+
+
   return (
     <div className="p-4">
       <div>
-        {JSON.stringify(categories, null, 2)}
+        {/* {JSON.stringify(categories, null, 2)} */}
+        {JSON.stringify(categories.data, null, 2)}
       </div>
       <div className="flex flex-col gap-y-4">
         <div>
