85f90f4e9299faae30f3d732c4c2010426018520
XoDeR <xoder.vs@gmail.com>
2025-08-22 20:08:58 +0200

    form for sign up using shared auth schema

---
+++ src/modules/auth/schemas.ts
@@ -0,0 +1,20 @@
+import z from "zod";
+
+export const registerSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(3),
+  username: z
+    .string()
+    // will be used in domain name like [username].shop.com
+    .min(3, "Username must be at least 3 characters")
+    .max(63, "Username must be less than 63 characters")
+    .regex(
+      /^[a-z0-9][a-z0-9-]*[a-z0-9]$/,
+      "Username can only contain lowercase letters, numbers and hyphens. It must start and end with a letter or a number"
+    )
+    .refine(
+      (val) => !val.includes("--"),
+      "Username cannot contain consecutive hyphens"
+    )
+    .transform((val) => val.toLowerCase()),
+});
