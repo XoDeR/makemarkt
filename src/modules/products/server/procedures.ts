dbad2d1cc960ea9983e6c0b404bd00ef427dc7cf
XoDeR <xoder.vs@gmail.com>
2025-09-11 19:51:14 +0200

    category loads just own products

--- src/modules/products/server/procedures.ts
+++ src/modules/products/server/procedures.ts
@@ -1,10 +1,38 @@
 import { baseProcedure, createTRPCRouter } from "@/trpc/init";
+import type { Where } from "payload";
+import { z } from "zod";
 
 export const productsRouter = createTRPCRouter({
-  getMany: baseProcedure.query(async ({ ctx }) => {
+  getMany: baseProcedure.input(z.object({
+    category: z.string().nullable().optional(),
+  })).query(async ({ ctx, input }) => {
+    const where: Where = {};
+
+    if (input.category) {
+      const categoriesData = await ctx.db.find({
+        collection: "categories",
+        limit: 1,
+        pagination: false,
+        where: {
+          slug: {
+            equals: input.category,
+          }
+        }
+      });
+
+      const category = categoriesData.docs[0];
+
+      if (category) {
+        where["category.slug"] = {
+          equals: category.slug,
+        }
+      }
+    }
+
     const data = await ctx.db.find({
       collection: "products",
       depth: 1, // Populate "category", "image"
+      where
     });
 
     return data;
