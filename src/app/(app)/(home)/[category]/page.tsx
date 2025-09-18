b346dda32eed8d4e2755a9955297897aa01d98e4
XoDeR <xoder.vs@gmail.com>
2025-09-18 17:36:13 +0200

    product filters: basic implementation

--- src/app/(app)/(home)/[category]/page.tsx
+++ src/app/(app)/(home)/[category]/page.tsx
@@ -2,6 +2,7 @@ import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 import { getQueryClient, trpc } from "@/trpc/server";
 import { ProductList, ProductListSkeleton } from "@/modules/products/ui/components/product-list";
 import { Suspense } from "react";
+import { ProductFilters } from "@/modules/products/ui/components/product-filters";
 
 interface Props {
   params: Promise<{
@@ -22,8 +23,8 @@ const Page = async ({ params }: Props) => {
       <div className="px-4 lg:px-12 py-8 flex flex-col gap-4">
         <div className="grid grid-cols-1 lg:grid-cols-6 xl:grid-cols-8 gap-y-6 gap-x-12">
           <div className="lg:col-span-2 xl:col-span-2">
-            <div className="border p-2">
-              Product Filters
+            <div>
+              <ProductFilters />
             </div>
           </div>
           <div className="lg:col-span-4 xl:col-span-6">
