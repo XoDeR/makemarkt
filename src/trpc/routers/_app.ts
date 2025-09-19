a9b2820d00893dcd052e1aefab2c60b4cc150773
XoDeR <xoder.vs@gmail.com>
2025-09-19 17:01:54 +0200

    added tags collection to payload cms

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -2,6 +2,7 @@ import { createTRPCRouter } from "../init";
 import { categoriesRouter } from "@/modules/categories/server/procedures";
 import { authRouter } from "@/modules/auth/server/procedures";
 import { productsRouter } from "@/modules/products/server/procedures";
+import { tagsRouter } from "@/modules/tags/server/procedures";
 export const appRouter = createTRPCRouter({
   // hello: baseProcedure
   //   .input(
@@ -17,6 +18,7 @@ export const appRouter = createTRPCRouter({
   auth: authRouter,
   products: productsRouter,
   categories: categoriesRouter,
+  tags: tagsRouter,
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
