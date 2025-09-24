78941c6ed9e07e8321c51a42ba112001aab34efc
XoDeR <xoder.vs@gmail.com>
2025-09-24 19:59:06 +0200

    updated product card with extra fields

--- src/modules/products/ui/components/product-card.tsx
+++ src/modules/products/ui/components/product-card.tsx
@@ -1,3 +1,4 @@
+import { StarIcon } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -29,9 +30,33 @@ export const ProductCard = ({
           <Image
             alt={name}
             fill
-            src={imageUrl || ""}
+            src={imageUrl || "/placeholder.png"}
             className="object-cover"
           />
+        </div>
+        <div className="p-4 border-y flex flex-col gap-3 flex-1">
+          <h2 className="text-lg font-medium line-clamp-4">{name}</h2>
+          {/* Should redirect to the user's shop */}
+          <div className="flex items-center gap-2" onClick={() => { }}>
+            {authorImageUrl && (
+              <Image
+                alt={authorUsername}
+                src={authorImageUrl}
+                width={16}
+                height={16}
+                className="rounded-full border shrink-0 size-[16px]"
+              />
+            )}
+            <p className="text-sm underline font-medium">{authorUsername}</p>
+          </div>
+          {reviewCount > 0 && (
+            <div className="flex items-center gap-1">
+              <StarIcon className="size-3.5 fill-black" />
+              <p className="text-sm font-medium">
+                {reviewRating} ({reviewCount})
+              </p>
+            </div>
+          )}
         </div>
       </div>
     </Link>
