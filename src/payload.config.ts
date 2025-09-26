730049fe633afabee638721e571f51248c8f299f
XoDeR <xoder.vs@gmail.com>
2025-09-26 23:04:05 +0200

    multi tenants implementation

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -12,6 +12,9 @@ import { Media } from './collections/Media'
 import { Categories } from './collections/Categories'
 import { Products } from './collections/Products'
 import { Tags } from './collections/Tags'
+import { Tenants } from './collections/Tenants'
+
+import { multiTenantPlugin } from '@payloadcms/plugin-multi-tenant'
 
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
@@ -23,7 +26,7 @@ export default buildConfig({
       baseDir: path.resolve(dirname),
     },
   },
-  collections: [Users, Media, Categories, Products, Tags],
+  collections: [Users, Media, Categories, Products, Tags, Tenants],
   editor: lexicalEditor(),
   secret: process.env.PAYLOAD_SECRET || '',
   typescript: {
@@ -36,5 +39,14 @@ export default buildConfig({
   plugins: [
     payloadCloudPlugin(),
     // storage-adapter-placeholder
+    multiTenantPlugin({
+      collections: {
+        products: {},
+      },
+      tenantsArrayField: {
+        includeDefaultField: false,
+      },
+      userHasAccessToAllTenants: (user) => Boolean(user?.roles?.includes("super-admin")),
+    }),
   ],
 })
