30ec0a6e24c66411cf87b37e9e7b633e9e3bb5aa
XoDeR <xoder.vs@gmail.com>
2025-09-27 19:23:26 +0200

    Show all products on a home page

--- src/app/(app)/(home)/page.tsx
+++ src/app/(app)/(home)/page.tsx
@@ -1,44 +1,69 @@
-"use client";
+//"use client";
 
-import { Button } from "@/components/ui/button";
-import { Checkbox } from "@/components/ui/checkbox";
-import { Input } from "@/components/ui/input";
-import { Progress } from "@/components/ui/progress";
-import { Textarea } from "@/components/ui/textarea";
-import { useTRPC } from "@/trpc/client";
-import { useQuery } from "@tanstack/react-query";
+// import { Button } from "@/components/ui/button";
+// import { Checkbox } from "@/components/ui/checkbox";
+// import { Input } from "@/components/ui/input";
+// import { Progress } from "@/components/ui/progress";
+// import { Textarea } from "@/components/ui/textarea";
+// import { useTRPC } from "@/trpc/client";
+// import { useQuery } from "@tanstack/react-query";
 
-export default function Home() {
-  const trpc = useTRPC();
-  const categories = useQuery(trpc.categories.getMany.queryOptions());
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { getQueryClient, trpc } from "@/trpc/server";
+import { SearchParams } from "nuqs/server";
+import { loadProductFilters } from "@/modules/products/search-params";
+import { ProductListView } from "@/modules/products/ui/views/product-list-view";
+import { DEFAULT_LIMIT } from "@/constants";
 
-  const { data } = useQuery(trpc.auth.session.queryOptions());
-
-  return (
-    <div className="p-4">
-      <div>
-        {/* {JSON.stringify(categories.data, null, 2)} */}
-        {JSON.stringify(data?.user, null, 2)}
-      </div>
-      <div className="flex flex-col gap-y-4">
-        <div>
-          <Button variant="elevated">
-            Button
-          </Button>
-        </div>
-        <div>
-          <Input placeholder="Input" />
-        </div>
-        <div>
-          <Progress value={50} />
-        </div>
-        <div>
-          <Textarea placeholder="Textarea" />
-        </div>
-        <div>
-          <Checkbox />
-        </div>
-      </div>
-    </div>
-  );
+interface Props {
+  searchParams: Promise<SearchParams>;
 }
+
+const Home = async ({ searchParams }: Props) => {
+  // const trpc = useTRPC();
+  // const categories = useQuery(trpc.categories.getMany.queryOptions());
+
+  // const { data } = useQuery(trpc.auth.session.queryOptions());
+
+  const filters = await loadProductFilters(searchParams);
+
+  const queryClient = getQueryClient();
+  void queryClient.prefetchInfiniteQuery(trpc.products.getMany.infiniteQueryOptions({
+    ...filters,
+    limit: DEFAULT_LIMIT,
+  }));
+
+  // return (
+  //   <div className="p-4">
+  //     <div>
+  //       {JSON.stringify(data?.user, null, 2)}
+  //     </div>
+  //     <div className="flex flex-col gap-y-4">
+  //       <div>
+  //         <Button variant="elevated">
+  //           Button
+  //         </Button>
+  //       </div>
+  //       <div>
+  //         <Input placeholder="Input" />
+  //       </div>
+  //       <div>
+  //         <Progress value={50} />
+  //       </div>
+  //       <div>
+  //         <Textarea placeholder="Textarea" />
+  //       </div>
+  //       <div>
+  //         <Checkbox />
+  //       </div>
+  //     </div>
+  //   </div>
+  // );
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <ProductListView />
+    </HydrationBoundary>
+  )
+}
+
+export default Home;
