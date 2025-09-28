8b3eb1d5d79486b8e2739ce7a52a4d7cd4c10279
XoDeR <xoder.vs@gmail.com>
2025-09-28 22:54:37 +0200

    navbar in layout for tenant pages

--- src/app/(app)/(tenants)/tenants/[slug]/(home)/layout.tsx
+++ src/app/(app)/(tenants)/tenants/[slug]/(home)/layout.tsx
@@ -1,3 +1,5 @@
+import { Navbar } from "@/modules/tenants/ui/components/navbar";
+
 interface Props {
   children: React.ReactNode;
   params: Promise<{ slug: string }>;
@@ -6,6 +8,7 @@ interface Props {
 const Layout = ({ children, params }: Props) => {
   return (
     <div className="min-h-screen bg-[#F4F4F0] flex flex-col">
+      <Navbar />
       {children}
     </div>
   );
