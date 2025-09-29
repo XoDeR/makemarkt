0158532bc85d3a612b3c28d313249b895a260ad2
XoDeR <xoder.vs@gmail.com>
2025-09-29 12:53:13 +0200

    improved ui: footer and layout

--- src/app/(app)/(tenants)/tenants/[slug]/(home)/layout.tsx
+++ src/app/(app)/(tenants)/tenants/[slug]/(home)/layout.tsx
@@ -1,3 +1,4 @@
+import { Footer } from "@/modules/tenants/ui/components/footer";
 import { Navbar } from "@/modules/tenants/ui/components/navbar";
 
 interface Props {
@@ -9,7 +10,12 @@ const Layout = ({ children, params }: Props) => {
   return (
     <div className="min-h-screen bg-[#F4F4F0] flex flex-col">
       <Navbar />
-      {children}
+      <div className="flex-1">
+        <div className="max-w-(--breakpoint-xl) mx-auto">
+          {children}
+        </div>
+      </div>
+      <Footer />
     </div>
   );
 }
