9cbce677099c7c59d0a44a3656d1ea23f5f97c1f
XoDeR <xoder.vs@gmail.com>
2025-10-02 23:00:07 +0200

    zustand's cart store

---
+++ src/modules/checkout/store/use-cart-store.ts
@@ -0,0 +1,67 @@
+import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
+
+// Each tenant has its own cart
+
+interface TenantCart {
+  productIds: string[];
+}
+
+interface CartState {
+  tenantCarts: Record<string, TenantCart>;
+  addProduct: (tenantSlug: string, productId: string) => void;
+  removeProduct: (tenantSlug: string, productId: string) => void;
+  clearCart: (tenantSlug: string) => void;
+  clearAllCarts: () => void;
+  getCartByTenant: (tenantSlug: string) => string[];
+}
+
+export const usecartStore = create<CartState>()(
+  persist(
+    (set, get) => ({
+      tenantCarts: {},
+      addProduct: (tenantSlug, productId) =>
+        set((state) => ({
+          tenantCarts: {
+            ...state.tenantCarts,
+            [tenantSlug]: {
+              productIds: [
+                ...(state.tenantCarts[tenantSlug]?.productIds || []),
+                productId,
+              ]
+            }
+          }
+        })),
+      removeProduct: (tenantSlug, productId) =>
+        set((state) => ({
+          tenantCarts: {
+            ...state.tenantCarts,
+            [tenantSlug]: {
+              productIds: state.tenantCarts[tenantSlug]?.productIds.filter(
+                (id) => id !== productId
+              ) || [],
+            }
+          }
+        })),
+      clearCart: (tenantSlug) =>
+        set((state) => ({
+          tenantCarts: {
+            ...state.tenantCarts,
+            [tenantSlug]: {
+              productIds: [],
+            }
+          }
+        })),
+      clearAllCarts: () =>
+        set({
+          tenantCarts: {},
+        }),
+      getCartByTenant: (tenantSlug) =>
+        get().tenantCarts[tenantSlug]?.productIds || [],
+    }),
+    {
+      name: "makemarkt-cart",
+      storage: createJSONStorage(() => localStorage), // localStorage is window.localStorage
+    },
+  ),
+);
