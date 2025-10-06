5db2b26f06c1a4dd2887a9660888d835259b88b3
XoDeR <xoder.vs@gmail.com>
2025-10-06 14:10:33 +0200

    checkout view: loading data and a check for errors

--- src/modules/checkout/ui/components/views/checkout-view.tsx
+++ src/modules/checkout/ui/components/views/checkout-view.tsx
@@ -1,14 +1,35 @@
 "use client";
 
+import { useCart } from "@/modules/checkout/hooks/use-cart";
+import { useTRPC } from "@/trpc/client";
+import { useQuery } from "@tanstack/react-query";
+import { useEffect } from "react";
+import { toast } from "sonner";
+
 interface Props {
   tenantSlug: string;
 }
 
 export const CheckoutView = ({ tenantSlug }: Props) => {
+  const { productIds, clearAllCarts } = useCart(tenantSlug);
+
+  const trpc = useTRPC();
+  const { data, error } = useQuery(trpc.checkout.getProducts.queryOptions({
+    ids: productIds,
+  }));
+
+  useEffect(() => {
+    if (error?.data?.code === "NOT_FOUND") {
+      clearAllCarts(); // removing invalid data
+      toast.warning("Invalid products found, cart cleared");
+    }
+  }, [error, clearAllCarts])
+
   return (
     <div>
       Checkout View
       {tenantSlug}
+      {JSON.stringify(data, null, 2)}
     </div>
   )
 }
