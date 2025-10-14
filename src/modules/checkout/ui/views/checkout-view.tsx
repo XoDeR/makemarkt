d26d6cd7bf23872abf085fbbec0a5bac71a6c67f
XoDeR <xoder.vs@gmail.com>
2025-10-14 17:11:02 +0200

    redirect to library with query invalidation (implemented todo) in checkout-view

--- src/modules/checkout/ui/views/checkout-view.tsx
+++ src/modules/checkout/ui/views/checkout-view.tsx
@@ -3,7 +3,7 @@
 import { generateTenantUrl } from "@/lib/utils";
 import { useCart } from "@/modules/checkout/hooks/use-cart";
 import { useTRPC } from "@/trpc/client";
-import { useMutation, useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { useEffect } from "react";
 import { toast } from "sonner";
 import { CheckoutItem } from "../components/checkout-item";
@@ -22,6 +22,7 @@ export const CheckoutView = ({ tenantSlug }: Props) => {
   const { productIds, removeProduct, clearCart } = useCart(tenantSlug);
 
   const trpc = useTRPC();
+  const queryClient = useQueryClient();
   const { data, error, isLoading } = useQuery(trpc.checkout.getProducts.queryOptions({
     ids: productIds,
   }));
@@ -46,12 +47,17 @@ export const CheckoutView = ({ tenantSlug }: Props) => {
     if (states.success) {
       setStates({ success: false, cancel: false });
       clearCart();
-
-      // TODO: invalidate library here
-
-      router.push("/products");
+      queryClient.invalidateQueries(trpc.library.getMany.infiniteQueryFilter());
+      router.push("/library");
     }
-  }, [states.success, clearCart, router, setStates]);
+  }, [
+    states.success,
+    clearCart,
+    router,
+    setStates,
+    queryClient,
+    trpc.library.getMany,
+  ]);
 
   useEffect(() => {
     if (error?.data?.code === "NOT_FOUND") {
