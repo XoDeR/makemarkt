fcf8f54317b4bdb90a54da6325b450f51ed53df8
XoDeR <xoder.vs@gmail.com>
2025-10-14 17:37:12 +0200

    get one trpc procedure for library router

--- src/modules/library/server/procedures.ts
+++ src/modules/library/server/procedures.ts
@@ -3,8 +3,55 @@ import type { Sort, Where } from "payload";
 import { DEFAULT_LIMIT } from "@/constants";
 import { Media, Tenant } from "@/payload-types";
 import { protectedProcedure, createTRPCRouter } from "@/trpc/init";
+import { TRPCError } from "@trpc/server";
 
 export const libraryRouter = createTRPCRouter({
+  getOne: protectedProcedure.input(z.object({
+    productId: z.string(),
+  })).query(async ({ ctx, input }) => {
+    const ordersData = await ctx.db.find({
+      collection: "orders",
+      limit: 1,
+      pagination: false,
+      where: {
+        and: [
+          {
+            product: {
+              equals: input.productId,
+            },
+          },
+          {
+            user: {
+              equals: ctx.session.user.id,
+            },
+          },
+        ],
+      },
+    });
+
+    const order = ordersData.docs[0];
+
+    if (!order) {
+      throw new TRPCError({
+        code: "NOT_FOUND",
+        message: "Order not found"
+      })
+    }
+
+    const product = await ctx.db.findByID({
+      collection: "products",
+      id: input.productId,
+    });
+
+    if (!product) {
+      throw new TRPCError({
+        code: "NOT_FOUND",
+        message: "Product not found"
+      })
+    }
+
+    return product;
+  }),
   getMany: protectedProcedure.input(z.object({
     cursor: z.number().default(1),
     limit: z.number().default(DEFAULT_LIMIT),
