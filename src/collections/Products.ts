f8a5465e26cfed69ea0eea898d5b20101a7b57c2
XoDeR <xoder.vs@gmail.com>
2025-10-21 22:16:32 +0200

    restricted access to cms tables

--- src/collections/Products.ts
+++ src/collections/Products.ts
@@ -1,7 +1,17 @@
+import { isSuperAdmin } from '@/lib/access'
+import { Tenant } from '@/payload-types';
 import type { CollectionConfig } from 'payload'
 
 export const Products: CollectionConfig = {
   slug: "products",
+  access: {
+    create: ({ req }) => {
+      if (isSuperAdmin(req.user)) return true;
+      const tenant = req.user?.tenants?.[0]?.tenant as Tenant;
+      return Boolean(tenant?.stripeDetailsSubmitted); // only if tenant has submitted stripe details
+    },
+    // The rest of the rules are already handled in payload config
+  },
   admin: {
     useAsTitle: "name",
   },
@@ -45,6 +55,14 @@ export const Products: CollectionConfig = {
       type: "select",
       options: ["30-day", "14-day", "7-day", "3-day", "1-day", "no-refunds"],
       defaultValue: "30-day",
+    },
+    {
+      name: "content",
+      type: "textarea",
+      admin: {
+        description:
+          "Protected content only visible to customers after purchase. Add documentation, files, guides and bonus materials. Supports markdown formatting"
+      },
     },
   ]
 }
