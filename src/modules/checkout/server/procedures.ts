f9aff5fc4700e27dec1751b534ee0b0592358be9
XoDeR <xoder.vs@gmail.com>
2025-10-29 18:38:34 +0100

    excluded archived products from checkout api

--- src/modules/checkout/server/procedures.ts
+++ src/modules/checkout/server/procedures.ts
@@ -74,7 +74,12 @@ export const checkoutRouter = createTRPCRouter({
               "tenant.slug": {
                 equals: input.tenantSlug
               }
-            }
+            },
+            {
+              isArchived: {
+                not_equals: true,
+              }
+            },
           ]
         },
       })
@@ -170,9 +175,18 @@ export const checkoutRouter = createTRPCRouter({
       collection: "products",
       depth: 2, // Populate "category", "image", "tenant" & "tenant.image"
       where: {
-        id: {
-          in: input.ids,
-        }
+        and: [
+          {
+            id: {
+              in: input.ids,
+            },
+          },
+          {
+            isArchived: {
+              not_equals: true,
+            }
+          },
+        ],
       },
     });
 
