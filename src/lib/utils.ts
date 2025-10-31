1da79db540b703f5a2c62af822f839f601a0c7d8
XoDeR <xoder.vs@gmail.com>
2025-10-31 18:24:11 +0100

    tenant url is different in production

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,7 +6,15 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function generateTenantUrl(tenantSlug: string) {
-  return `/tenants/${tenantSlug}`;
+  if (process.env.NODE_ENV === "development") {
+    return `/tenants/${tenantSlug}`;
+  }
+
+  const protocol = "https";
+  const domain = process.env.NEXT_PUBLIC_ROOT_DOMAIN!;
+
+  // example: https://john.makemarkt.com
+  return `${protocol}://${tenantSlug}.${domain}`;
 }
 
 export function formatCurrency(value: number | string) {
