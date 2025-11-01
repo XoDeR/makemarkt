20dc2b3ef8b53e62c46073a8d34867aef0284421
XoDeR <xoder.vs@gmail.com>
2025-11-01 15:25:50 +0100

    corrected other redirects

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,13 +6,19 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function generateTenantUrl(tenantSlug: string) {
+  // in dev mode use normal routing
   if (process.env.NODE_ENV === "development") {
-    return `/tenants/${tenantSlug}`;
+    return `${process.env.NEXT_PUBLIC_APP_URL}/tenants/${tenantSlug}`;
   }
 
-  const protocol = "https";
+  let protocol = "https";
   const domain = process.env.NEXT_PUBLIC_ROOT_DOMAIN!;
 
+  // if (process.env.NODE_ENV === "development") {
+  //   protocol = "http";
+  // }
+
+  // in prod use subdomain routing
   // example: https://john.makemarkt.com
   return `${protocol}://${tenantSlug}.${domain}`;
 }
