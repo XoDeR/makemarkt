4833044efce898b8193180e5de8961617738b7c4
XoDeR <xoder.vs@gmail.com>
2025-11-02 17:45:35 +0100

    added routing condition

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,8 +6,11 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function generateTenantUrl(tenantSlug: string) {
-  // in dev mode use normal routing
-  if (process.env.NODE_ENV === "development") {
+  const isDevelopment = process.env.NODE_ENV === "development";
+  const isSubdomainRoutingEnabled = Boolean(process.env.NEXT_PUBLIC_ENABLE_SUBDOMAIN_ROUTING!);
+
+  // in dev or subdomain routing disabled mode use normal routing
+  if (isDevelopment || !isSubdomainRoutingEnabled) {
     return `${process.env.NEXT_PUBLIC_APP_URL}/tenants/${tenantSlug}`;
   }
 
